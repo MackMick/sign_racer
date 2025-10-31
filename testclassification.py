ccb06e41e520d19fef6b2c10adb7032953b8ea19
strombologni <mackmick.lidman@gmail.com>
2025-10-31 18:23:52 +0100

    colors+acc

--- testclassification.py
+++ testclassification.py
@@ -14,8 +14,23 @@ FONT_SIZE = 1
 FONT_THICKNESS = 4
 HANDEDNESS_TEXT_COLOR = (88, 205, 54) # vibrant green
 
+correct_string = "THIS IS A SAMPLE"
+current_pos = 0
+colorvector = [0] * len(correct_string) # makes empty array -> 
+#0 -> not yet seen, 1 -> correct, 2 -> wrong
+#we should really make this a real lookup thing instead of just arbitrarily ascribing colors to numbers
 
 fullstring = ""
+
+skinny_set = {"I"}
+
+adjusted_placements = [20] # list to make sure all letters are placed in a good manner -> distinguish between thin and wide letters
+for pos in range(1,len(correct_string)):
+    if correct_string[pos- 1] in skinny_set:
+        adjusted_placements.append(15)
+    else:
+        adjusted_placements.append(40)
+
 
 model = ASL_MLP()
 model.load_state_dict(torch.load("asl_mlp_model25.pth", weights_only=True))
@@ -80,33 +95,79 @@ def draw_landmarks_on_image(rgb_image, detection_result: mp.tasks.vision.HandLan
       return rgb_image
 
 
-def write_text_on_image(frame, text):
+def write_text_on_image(frame, text=""):
     font                   = cv2.FONT_HERSHEY_SIMPLEX
     bottomLeftCornerOfText = (250,250)
-    fontScale              = 1
+    fontScale              = 2
     fontColor              = (255,255,255)
-    thickness              = 1
+    thickness              = 3
     lineType               = 2
 
     global fullstring
+    global correct_string
+    global adjusted_placements
 
-    cv2.putText(frame,text, 
-        bottomLeftCornerOfText, 
-        font, 
-        fontScale,
-        fontColor,
-        thickness,
-        lineType)
+    if len(text) > 0:
+        if text == " ":
+            text = "(SPACE)"
+        cv2.putText(frame,text, 
+            bottomLeftCornerOfText, 
+            font, 
+            fontScale,
+            fontColor,
+            thickness,
+            lineType)
     
-    longcolor = (255,0,0)
-    textPlacement = (150, 150)
-    cv2.putText(frame,fullstring, 
-        textPlacement, 
-        font, 
-        fontScale,
-        longcolor,
-        thickness,
-        lineType)
+    #TO ADD: check here if it is the correct letter -> different color if wrong
+
+    right_color = (0,255,0) #green
+    wrong_color = (0,0,255) #red
+    current_color = (255,255,0) #blue
+    coming_color = (255,0,0) #
+
+    textPlacement = (10, 450) #(0,0) = topleft, (x,y)
+    
+    accumulated_pos = 0
+    for pos, char in enumerate(correct_string):
+        accumulated_pos += adjusted_placements[pos]
+        adjusted_placement = (textPlacement[0] + accumulated_pos, textPlacement[1])
+
+        if pos == len(fullstring):
+            color = current_color
+        elif colorvector[pos] == 0:
+            color = coming_color
+        elif colorvector[pos] == 1:
+            color = right_color
+        elif colorvector[pos] == 2:
+            color = wrong_color
+        else:
+            print("Color Exception")
+        
+        if char == " ":
+            char = "_"
+
+        cv2.putText(frame,char, 
+            adjusted_placement, 
+            font, 
+            fontScale,
+            color,
+            thickness,
+            lineType)
+    
+    if len(fullstring) == len(correct_string):
+        accuracy = colorvector.count(1)/len(colorvector) * 100
+        resultstext = f"your final accuracy was: {accuracy}%"
+        resultsplacement = (50,50)
+        resultsscale = 1
+        resultcolor = (0,255,255)
+
+        cv2.putText(frame,resultstext, 
+            resultsplacement, 
+            font, 
+            resultsscale,
+            resultcolor,
+            thickness,
+            lineType)
 
     return frame
 
@@ -118,13 +179,13 @@ def predict(model, sample):
         output = model(sample)
         predicted_class = torch.argmax(output, dim=1).item()
         translated_prediction = translation_dict[predicted_class]
-        print(translated_prediction)
         return translated_prediction
 
 def predict_letter(frame, result):
     "takes the current hand object extruded from the frame and draws the model prediction on the screen"
     try:
         if result.hand_world_landmarks == []:
+            frame = write_text_on_image(frame) #only draw saved part
             return frame
         else:
             #transform the landmarks to the same format as the classifier is used to
@@ -133,15 +194,24 @@ def predict_letter(frame, result):
             scale = np.linalg.norm(landmarks[9] - landmarks[0])  # scales mean bone length
             landmarks /= scale
             global fullstring
+            global correct_string
+            global colorvector
 
             landmarks = landmarks.flatten() # is this right?
             prediction = predict(model, landmarks)
 
 
-            if cv2.waitKey(1) == ord('a'): #change this to blinking somehow
+            if cv2.waitKey(1) == ord('a') and len(fullstring) <= len(correct_string): #change this to blinking somehow
+    
                 fullstring += prediction
-                if prediction == " ":
-                    fullstring = ""
+
+                if prediction == correct_string[len(fullstring)-1]:
+                    colorvector[len(fullstring)-1] = 1
+                else:
+                    colorvector[len(fullstring)-1] = 2
+                print(colorvector)
+
+
 
             frame = write_text_on_image(frame,prediction) #draw the predicted letter on the image
         return frame #add the things to the frame properly
